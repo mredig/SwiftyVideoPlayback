012ae86321d5fac902bb446a2bb73e4d133514b4
Michael Redig <mredig@gmail.com>
2020-12-17 12:45:36 -0600

    (feat) added icon button

---
+++ Sources/SwiftyVideoPlayback/Controls/IconButton.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+class IconButton: UIButton {
+
+	enum Symbol: String {
+		case play = "play"
+		case playFill = "play.fill"
+		case playSlash = "play.slash"
+		case playSlashFill = "play.slash.fill"
+		case playPause = "playpause"
+		case playPauseFill = "playpause.fill"
+		case pause = "pause"
+		case pauseFill = "pause.fill"
+		case backward = "backward"
+		case backwardFill = "backward.fill"
+		case forward = "forward"
+		case forwardFill = "forward.fill"
+	}
+
+	var icon: Symbol {
+		didSet {
+			updateImages()
+		}
+	}
+
+	private var observer: NSKeyValueObservation?
+
+	init(icon: Symbol) {
+		self.icon = icon
+		super.init(frame: .zero)
+		updateImages()
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+
+	private func updateImages() {
+		let image = UIImage(systemName: icon.rawValue)//?.withTintColor(.secondarySystemBackground)
+		setImage(image, for: .normal)
+		contentHorizontalAlignment = .fill
+		contentVerticalAlignment = .fill
+
+		imageView?.contentMode = .scaleAspectFit
+		tintColor = .secondarySystemBackground
+	}
+}
